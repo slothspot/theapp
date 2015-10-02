12b88697d0ddc6f27d6f9fe8cff12bff84a25afe
Dmitry Melnichenko <dmytro.i.am@gmail.com>
2015-10-02 21:00:50 +0300

    Add endpoint to serve public resources and add mongodb connection

--- src/main/scala/name.dmitrym/theapp/TheApp.scala
+++ src/main/scala/name.dmitrym/theapp/TheApp.scala
@@ -2,23 +2,33 @@ package name.dmitrym.theapp
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
 
 import scala.io.StdIn
 
-object TheApp extends App {
+object TheApp extends App with LazyLogging {
   implicit val system = ActorSystem("theApp")
   implicit val materializer = ActorMaterializer()
 
-  val route = (path("hello") & get) {
-    complete("ok")
+  val route = path("public" / Rest) { r =>
+    getFromResource("public/" + r)
+  } ~ path("public") {
+    redirect("public/index.html", StatusCodes.MovedPermanently)
   }
 
-  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+  val config = ConfigFactory.load("application.conf")
+  assert(config.hasPath("service.interface"), "Config doesn't have service.interface specified")
+  val listenInterface = config.getString("service.interface")
+  assert(config.hasPath("service.port"), "Config doesn't have service.port specified")
+  val listenPort = config.getInt("service.port")
+  val bindingFuture = Http().bindAndHandle(route, listenInterface, listenPort)
 
   StdIn.readLine()
 
   import system.dispatcher
-  bindingFuture.flatMap(_.unbind()).onComplete( _ =>system.shutdown() )
+  bindingFuture.flatMap(_.unbind()).onComplete( _ => system.shutdown() )
 }

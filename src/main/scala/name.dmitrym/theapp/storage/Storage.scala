6c2de6bca59e830fc481da92ed5260fc05478c45
Dmitry Melnichenko <dmytro.i.am@gmail.com>
2015-11-16 23:56:42 +0200

    Add caching of db connections

--- src/main/scala/name.dmitrym/theapp/storage/Storage.scala
+++ src/main/scala/name.dmitrym/theapp/storage/Storage.scala
@@ -2,8 +2,10 @@ package name.dmitrym.theapp.models
 
 import com.mongodb.casbah.{MongoClientURI, MongoClient}
 import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
 
-class Storage private(mongoUri: String) {
+class Storage private(mongoUri: String) extends LazyLogging {
+  logger.debug(s"New instance of Storage created with uri $mongoUri")
   private[this] val mongoClient = MongoClient(MongoClientURI(mongoUri))
   private[this] val database = mongoClient("theapp")
 
@@ -11,11 +13,15 @@ class Storage private(mongoUri: String) {
   val companies = database("companies")
   val users = database("users")
   val invoices = database("invoices")
+
+  val oplog = database("oplog")
 }
 
 object Storage {
+  private[this] val cache = Map[Config, Storage]()
+
   def apply(cfg: Config):Storage = {
     assert(cfg.hasPath("mongo.uri"), "Config doesn't have mongo.uri specified")
-    new Storage(cfg.getString("mongo.uri"))
+    cache.getOrElse(cfg, new Storage(cfg.getString("mongo.uri")))
   }
 }
